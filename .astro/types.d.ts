01c7c959d18f496edd94e50eeecf62c0d2914b84
Joe Nyzio <jdnyzio@gmail.com>
2023-11-25 11:28:40 -0500

    blog

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -523,6 +523,668 @@ declare module 'astro:content' {
   data: any
 } & { render(): Render[".md"] };
 };
+"integrations": {
+"activecampaign.md": {
+	id: "activecampaign.md";
+  slug: "activecampaign";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"airtable.md": {
+	id: "airtable.md";
+  slug: "airtable";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"amplitude.md": {
+	id: "amplitude.md";
+  slug: "amplitude";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"asana.md": {
+	id: "asana.md";
+  slug: "asana";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"baremetrics.md": {
+	id: "baremetrics.md";
+  slug: "baremetrics";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"basecamp.md": {
+	id: "basecamp.md";
+  slug: "basecamp";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"big-commerce.md": {
+	id: "big-commerce.md";
+  slug: "big-commerce";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"buffer.md": {
+	id: "buffer.md";
+  slug: "buffer";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"calendly.md": {
+	id: "calendly.md";
+  slug: "calendly";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"campignmonitor.md": {
+	id: "campignmonitor.md";
+  slug: "campignmonitor";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"captivate.md": {
+	id: "captivate.md";
+  slug: "captivate";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"chartmogul.md": {
+	id: "chartmogul.md";
+  slug: "chartmogul";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"churnbuster.md": {
+	id: "churnbuster.md";
+  slug: "churnbuster";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"circle.md": {
+	id: "circle.md";
+  slug: "circle";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"clickup.md": {
+	id: "clickup.md";
+  slug: "clickup";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"convertkit.md": {
+	id: "convertkit.md";
+  slug: "convertkit";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"coppercrm.md": {
+	id: "coppercrm.md";
+  slug: "coppercrm";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"cove.md": {
+	id: "cove.md";
+  slug: "cove";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"data-studio.md": {
+	id: "data-studio.md";
+  slug: "data-studio";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"discord.md": {
+	id: "discord.md";
+  slug: "discord";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"disqus.md": {
+	id: "disqus.md";
+  slug: "disqus";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"doodle.md": {
+	id: "doodle.md";
+  slug: "doodle";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"drift.md": {
+	id: "drift.md";
+  slug: "drift";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"email-octopus.md": {
+	id: "email-octopus.md";
+  slug: "email-octopus";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"facebook.md": {
+	id: "facebook.md";
+  slug: "facebook";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"fathom-analytics.md": {
+	id: "fathom-analytics.md";
+  slug: "fathom-analytics";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"firstpromotor.md": {
+	id: "firstpromotor.md";
+  slug: "firstpromotor";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"flock.md": {
+	id: "flock.md";
+  slug: "flock";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"freshdesk.md": {
+	id: "freshdesk.md";
+  slug: "freshdesk";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"ghost.md": {
+	id: "ghost.md";
+  slug: "ghost";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"github.md": {
+	id: "github.md";
+  slug: "github";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"gitlab.md": {
+	id: "gitlab.md";
+  slug: "gitlab";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"google-adsense.md": {
+	id: "google-adsense.md";
+  slug: "google-adsense";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"google-analytics.md": {
+	id: "google-analytics.md";
+  slug: "google-analytics";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"google-docs.md": {
+	id: "google-docs.md";
+  slug: "google-docs";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"google-forms.md": {
+	id: "google-forms.md";
+  slug: "google-forms";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"googleadmanager.md": {
+	id: "googleadmanager.md";
+  slug: "googleadmanager";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"groove.md": {
+	id: "groove.md";
+  slug: "groove";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"gumroad.md": {
+	id: "gumroad.md";
+  slug: "gumroad";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"heap.md": {
+	id: "heap.md";
+  slug: "heap";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"help-scout.md": {
+	id: "help-scout.md";
+  slug: "help-scout";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"helpjuice.md": {
+	id: "helpjuice.md";
+  slug: "helpjuice";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"hotjar.md": {
+	id: "hotjar.md";
+  slug: "hotjar";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"hubspot.md": {
+	id: "hubspot.md";
+  slug: "hubspot";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"instagram.md": {
+	id: "instagram.md";
+  slug: "instagram";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"intercom.md": {
+	id: "intercom.md";
+  slug: "intercom";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"intravert.md": {
+	id: "intravert.md";
+  slug: "intravert";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"kayako.md": {
+	id: "kayako.md";
+  slug: "kayako";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"keen-io.md": {
+	id: "keen-io.md";
+  slug: "keen-io";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"live-chat.md": {
+	id: "live-chat.md";
+  slug: "live-chat";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"looker.md": {
+	id: "looker.md";
+  slug: "looker";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"loom.md": {
+	id: "loom.md";
+  slug: "loom";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"mailchimp.md": {
+	id: "mailchimp.md";
+  slug: "mailchimp";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"mailer-lite.md": {
+	id: "mailer-lite.md";
+  slug: "mailer-lite";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"matomo.md": {
+	id: "matomo.md";
+  slug: "matomo";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"microsoft-teams.md": {
+	id: "microsoft-teams.md";
+  slug: "microsoft-teams";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"miro.md": {
+	id: "miro.md";
+  slug: "miro";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"mixpanel.md": {
+	id: "mixpanel.md";
+  slug: "mixpanel";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"monday.md": {
+	id: "monday.md";
+  slug: "monday";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"notion.md": {
+	id: "notion.md";
+  slug: "notion";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"patreon.md": {
+	id: "patreon.md";
+  slug: "patreon";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"paypal.md": {
+	id: "paypal.md";
+  slug: "paypal";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"pinterest.md": {
+	id: "pinterest.md";
+  slug: "pinterest";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"pipedrive.md": {
+	id: "pipedrive.md";
+  slug: "pipedrive";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"placid.md": {
+	id: "placid.md";
+  slug: "placid";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"plausible.md": {
+	id: "plausible.md";
+  slug: "plausible";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"powerbi.md": {
+	id: "powerbi.md";
+  slug: "powerbi";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"salesforce.md": {
+	id: "salesforce.md";
+  slug: "salesforce";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"segment.md": {
+	id: "segment.md";
+  slug: "segment";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"shopify.md": {
+	id: "shopify.md";
+  slug: "shopify";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"simple-analytics.md": {
+	id: "simple-analytics.md";
+  slug: "simple-analytics";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"slack.md": {
+	id: "slack.md";
+  slug: "slack";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"snipcart.md": {
+	id: "snipcart.md";
+  slug: "snipcart";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"stripe.md": {
+	id: "stripe.md";
+  slug: "stripe";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"tableau.md": {
+	id: "tableau.md";
+  slug: "tableau";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"talkyard.md": {
+	id: "talkyard.md";
+  slug: "talkyard";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"tidio.md": {
+	id: "tidio.md";
+  slug: "tidio";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"transistor.md": {
+	id: "transistor.md";
+  slug: "transistor";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"trello.md": {
+	id: "trello.md";
+  slug: "trello";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"typeform.md": {
+	id: "typeform.md";
+  slug: "typeform";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"vimeo.md": {
+	id: "vimeo.md";
+  slug: "vimeo";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"viralloops.md": {
+	id: "viralloops.md";
+  slug: "viralloops";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"weglot.md": {
+	id: "weglot.md";
+  slug: "weglot";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"x.md": {
+	id: "x.md";
+  slug: "x";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"youtube copy.md": {
+	id: "youtube copy.md";
+  slug: "youtube-copy";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"youtube.md": {
+	id: "youtube.md";
+  slug: "youtube";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"zapier.md": {
+	id: "zapier.md";
+  slug: "zapier";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"zendesk.md": {
+	id: "zendesk.md";
+  slug: "zendesk";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"zohocrm.md": {
+	id: "zohocrm.md";
+  slug: "zohocrm";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+"zoom.md": {
+	id: "zoom.md";
+  slug: "zoom";
+  body: string;
+  collection: "integrations";
+  data: any
+} & { render(): Render[".md"] };
+};
+"posts": {
+"1.md": {
+	id: "1.md";
+  slug: "1";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"2.md": {
+	id: "2.md";
+  slug: "2";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"3.md": {
+	id: "3.md";
+  slug: "3";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"4.md": {
+	id: "4.md";
+  slug: "4";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+};
 
 	};
 
